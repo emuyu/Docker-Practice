9e26e0fec22288a6d38c31dab01ba56b888adcb7
matchan1201 <bumpakano.1@gmail.com>
2023-03-29 03:16:51 +0900

    初回作業・Readme整備

---
+++ mysql/sql/init.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE IF NOT EXISTS sample;
+
+CREATE TABLE user(
+    user_id serial primary key,
+    user_name char(16) NOT NULL,
+    password char(16) NOT NULL
+);
+
+INSERT INTO user(user_name, password) VALUES('user', 'password');
